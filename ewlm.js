df24912eb7ffc5f0ca836dd0e8a9e86cc84f6674
agnesmardh <agnes.mardh@gmail.com>
2016-01-08 14:32:37 +0100

    Added checkbox for completed wishes

--- ewlm.js
+++ ewlm.js
@@ -4,7 +4,19 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     wishes: function () {
-      return Wishes.find ({});
+       if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Wishes.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Wishes.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Wishes.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -24,7 +36,10 @@ if (Meteor.isClient) {
  
       // Clear form
       event.target.text.value = "";
-    }
+    },
+     "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked); 
+    }  
   });
 
 Template.wish.events({
